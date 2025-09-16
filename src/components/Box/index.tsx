6944554a9eb5b54fda3449f54796847af9d3db1f
Eda1000 <eedaah2024@gmail.com>
2025-09-15 23:21:30 -0300

    Updated index.tsx

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,33 +1,27 @@
 import React, { useState } from "react";
 import { FaCheck } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
-
-import boxImg from "../../assets/box/box2.png";
+import boxImg from "../../assets/box.png";
 import { Container, BoxImage, Button } from "./styles";
+
+// Corrige tipagem do ícone
+const CheckIcon = FaCheck as unknown as React.FC;
 
 const Box: React.FC = () => {
   const [selected, setSelected] = useState(false);
-  const navigate = useNavigate();
 
   const handleSelect = () => {
     setSelected(!selected);
-    toast.success("Caixa selecionada!");
   };
 
   const handleNext = () => {
-    if (!selected) {
-      toast.error("Selecione a caixa primeiro!");
-      return;
-    }
-    navigate("/home");
+    console.log("Próximo box selecionado");
   };
 
   return (
     <Container>
       <BoxImage src={boxImg} alt="Box" />
       <Button selected={selected} onClick={handleSelect}>
-        <FaCheck /> Selecionar
+        <CheckIcon /> Selecionar
       </Button>
       <Button onClick={handleNext}>Próximo</Button>
     </Container>
