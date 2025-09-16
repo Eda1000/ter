fbf8acbd382ea5bd69a9d25589841cc15f09bd9d
Eda1000 <eedaah2024@gmail.com>
2025-09-15 23:13:57 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,13 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-import Box from "components/Box";
-
-const Home = () => {
-  return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Página Home</h1>
-      <p>Bem-vindo ao sistema 🚀</p>
-    </div>
-  );
-};
+import Box from "./components/Box";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Box />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/next" element={<h1>Próxima Página</h1>} />
       </Routes>
-
-      {/* Container para os toasts */}
-      <ToastContainer />
     </BrowserRouter>
   );
 }
