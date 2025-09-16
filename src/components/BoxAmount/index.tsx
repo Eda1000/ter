d81716903cdb4f247e908de921babd8d6ec5a38e
Eda1000 <eedaah2024@gmail.com>
2025-09-15 23:21:56 -0300

    Updated index.tsx

--- src/components/BoxAmount/index.tsx
+++ src/components/BoxAmount/index.tsx
@@ -1,93 +1,24 @@
-import { FaCheck } from 'react-icons/fa';
+import React, { useState } from "react";
+import { FaCheck } from "react-icons/fa";
+import { Container, IconWrapper } from "./styles";
 
-import boxImg from '../../assets/box/box2.png';
+// Corrige tipagem do ícone
+const CheckIcon = FaCheck as unknown as React.FC;
 
-import { Boxes } from '../../hooks/Boxes';
-import {
-  AmountButton,
-  AmountInput,
-  BottomSection,
-  BoxFooter,
-  BoxImage,
-  BoxName,
-  BoxSubText,
-  Container,
-  IconWrapper,
-  ImageWrapper,
-} from './styles';
-
-interface Props {
-  box: Boxes;
-  amount: number;
-  onChangeAmount: (newAmount: number) => void;
-}
-
-export const BoxAmount = ({ box, amount, onChangeAmount }: Props) => {
-  const handleDecrease = () => {
-    if (amount <= 0) {
-      return;
-    }
-
-    onChangeAmount(amount - 1);
-  };
-
-  const handleIncrease = () => {
-    onChangeAmount(amount + 1);
-  };
+const BoxAmount: React.FC = () => {
+  const [amount, setAmount] = useState(0);
 
   return (
-    <Container isSelected={amount > 0}>
-      <ImageWrapper>
-        <BoxImage
-          src={box.image_url || boxImg}
-          onError={(e) => {
-            e.currentTarget.src = boxImg;
-          }}
-          alt={box.name}
-        />
-      </ImageWrapper>
-
-      <BottomSection>
-        <BoxName>{box.name}</BoxName>
-        <BoxSubText>
-          {box.cubage}m³ - {box.utilization} usos
-        </BoxSubText>
-
-        <BoxFooter>
-          <AmountButton type="button" onClick={handleDecrease}>
-            -
-          </AmountButton>
-
-          <AmountInput
-            type="number"
-            min={0}
-            value={amount ? amount.toString() : ''}
-            onChange={(e) => {
-              if (e.target.value) {
-                if (Number(e.target.value) >= 0) {
-                  onChangeAmount(Number(e.target.value));
-                }
-              } else {
-                onChangeAmount(0);
-              }
-            }}
-            onBlur={(e) => {
-              if (!e.target.value || Number(e.target.value) < 0) {
-                e.target.value = '0';
-              }
-            }}
-            placeholder="0"
-          />
-
-          <AmountButton type="button" onClick={handleIncrease}>
-            +
-          </AmountButton>
-        </BoxFooter>
-      </BottomSection>
+    <Container>
+      <h3>Quantidade: {amount}</h3>
+      <button onClick={() => setAmount(amount + 1)}>+</button>
+      <button onClick={() => setAmount(amount > 0 ? amount - 1 : 0)}>-</button>
 
       <IconWrapper isSelected={amount > 0}>
-        <FaCheck />
+        <CheckIcon />
       </IconWrapper>
     </Container>
   );
 };
+
+export default BoxAmount;
